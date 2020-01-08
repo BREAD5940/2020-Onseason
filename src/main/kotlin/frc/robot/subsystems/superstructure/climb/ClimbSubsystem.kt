17a1b3cade740f48a75c8351ec950aee73b37b4c
Max Goldenson <47581749+GAM3TIN@users.noreply.github.com>
2020-01-08 11:21:01 -0800

    okie da solinoinundeie on da climbbrer is whourking mees thinkes

--- src/main/kotlin/frc/robot/subsystems/superstructure/climb/ClimbSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/superstructure/climb/ClimbSubsystem.kt
@@ -1,18 +1,11 @@
 package frc.robot.subsystems.superstructure.climb
-
-import com.ctre.phoenix.motorcontrol.InvertType
 import com.revrobotics.CANSparkMaxLowLevel
-import frc.robot.subsystems.superstructure.Proximal
 import org.ghrobotics.lib.commands.FalconCommand
-import org.ghrobotics.lib.commands.FalconSubsystem
 import org.ghrobotics.lib.mathematics.units.*
-import org.ghrobotics.lib.mathematics.units.derived.Radian
-import org.ghrobotics.lib.mathematics.units.derived.degrees
 import org.ghrobotics.lib.mathematics.units.nativeunit.*
-import org.ghrobotics.lib.motors.ctre.FalconSRX
-import org.ghrobotics.lib.motors.ctre.falconSRX
 import org.ghrobotics.lib.motors.rev.FalconMAX
 import org.ghrobotics.lib.wrappers.FalconDoubleSolenoid
+import org.ghrobotics.lib.wrappers.FalconSolenoid
 
 object ClimbSubsystem {
 
@@ -21,11 +14,12 @@ object ClimbSubsystem {
         //TODO Tune PID / Enter Values
         val Kp = 0.00
         val Kd = 0.00
-        val solenoid = FalconDoubleSolenoid(0,1, 8)
-        //This code was wrote assuming we are useing a "yeet stick" that pulls us up the poll
+        val solenoid = FalconDoubleSolenoid(0, 1, 8)
+        //This code was wrote assuming we are useing a "yeet stick" that pulls us up the poll TODO Tune this!!
         val climbMaster: FalconMAX<Meter> = FalconMAX(21, CANSparkMaxLowLevel.MotorType.kBrushless, NativeUnitLengthModel(4096.nativeUnits, 0.75.inches))
         val climbSlave = FalconMAX(1   /*CHANGE PORT*/, CANSparkMaxLowLevel.MotorType.kBrushless, DefaultNativeUnitModel)
-        init{
+
+        init {
             //TODO CHECK FOR INVERT!
             climbMaster.canSparkMax.restoreFactoryDefaults()
             climbSlave.canSparkMax.restoreFactoryDefaults()
@@ -36,20 +30,23 @@ object ClimbSubsystem {
             climbMaster.canSparkMax.pidController.setD(1.0, 1)
             climbSlave.canSparkMax.pidController.setP(1.0, 1)
             climbMaster.canSparkMax.pidController.setD(1.0, 1)
-            
+
         }
-        override fun initialize(){
+
+        override fun initialize() {
             climbMaster.setPosition(climbHeight)
         }
 
         override fun isFinished(): Boolean {
-            return (climbMaster.encoder.position -climbHeight).absoluteValue < 0.05.meters
-
+            if((climbMaster.encoder.position - climbHeight).absoluteValue < 0.05.meters) {
+                solenoid.state = FalconSolenoid.State.Reverse
+                return true
+            } else {
+                return false
+            }
         }
 
     }
-
-
 }
 
 
