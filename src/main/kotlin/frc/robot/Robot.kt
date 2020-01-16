fd156a93e65338bce8d303d95b88523008edf20d
dmc-account <55603701+dmc-account@users.noreply.github.com>
2020-01-16 09:57:53 -0800

    Add first color sensor code (with errors)

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import frc.robot.autonomous.Autonomous
 import frc.robot.subsystems.drive.DriveSubsystem
+import frc.robot.subsystems.sensors.Sensors
 import org.ghrobotics.lib.wrappers.FalconTimedRobot
 
 object Robot : FalconTimedRobot() {
@@ -33,6 +34,7 @@ object Robot : FalconTimedRobot() {
         Autonomous.update()
         Controls.update()
         Network.update()
+        Sensors.update()
     }
 
     override fun disabledInit() {
