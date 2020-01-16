1d4bafd9df92602284ace3ae0a6747f2e73cad3a
dmc-account <55603701+dmc-account@users.noreply.github.com>
2020-01-16 10:17:24 -0800

    changed from Java to Kotlin, everything works except ColorSensorV3

--- src/main/kotlin/frc/robot/subsystems/sensors/Sensors.kt
+++ src/main/kotlin/frc/robot/subsystems/sensors/Sensors.kt
@@ -17,22 +17,21 @@ object Sensors {
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-    class Robot{
+     class Robot : TimedRobot() {
         /**
          * Change the I2C port below to match the connection of your color sensor
          */
-        private final I2C.Port i2cPort = I2C.Port.kOnboard;
+        private val i2cPort = I2C.Port.kOnboard
 
         /**
          * A Rev Color Sensor V3 object is constructed with an I2C port as a
          * parameter. The device will be automatically initialized with default
          * parameters.
          */
-        private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+        private val m_colorSensor = ColorSensorV3(i2cPort)
 
-        @Override
-        public robotPeriodic() {
-            /*
+        override fun robotPeriodic() {
+            /**
              * The method GetColor() returns a normalized color value from the sensor and can be
              * useful if outputting the color to an RGB LED or similar. To
              * read the raw color, use GetRawColor().
@@ -40,42 +39,28 @@ object Sensors {
              * The color sensor works best when within a few inches from an object in
              * well lit conditions (the built in LED is a big help here!). The farther
              * an object is the more light from the surroundings will bleed into the
-             *measurements and make it difficult to accurately determine its color.*/
-            
-            Color detectedColor = m_colorSensor.getColor();
+             * measurements and make it difficult to accurately determine its color.
+             */
+            val detectedColor = m_colorSensor.getColor()
 
-            /*
+            /**
              * The sensor returns a raw IR value of the infrared light detected.
              */
-            val m_colorSensor
-            
-            var IR = m_colorSensor.getIR()
+            val IR = m_colorSensor.getIR()
 
-            /*
+            /**
              * Open Smart Dashboard or Shuffleboard to see the color detected by the
              * sensor.
              */
-            var detectedColor
             SmartDashboard.putNumber("Red", detectedColor.red)
             SmartDashboard.putNumber("Green", detectedColor.green)
             SmartDashboard.putNumber("Blue", detectedColor.blue)
-            SmartDashboard.putNumber("IR", IR);
+            SmartDashboard.putNumber("IR", IR)
 
-            val m_colorSensor  
-            /*
-             * In addition to RGB IR values, the color sensor can also return an
-             * infrared proximity value. The chip contains an IR led which will emit
-             * IR pulses and measure the intensity of the return. When an object is
-             * close the value of the proximity will be large (max 2047 with default
-             * settings) and will approach zero when the object is far away.
-             *
-             * Proximity can be used to roughly approximate the distance of an object
-             * or provide a threshold for when an object is close enough to provide
-             * accurate color values.
-             */
-            var proximity = m_colorSensor.getProximity();
+            val proximity = m_colorSensor.getProximity()
 
-            SmartDashboard.putNumber("Proximity", proximity);
+            SmartDashboard.putNumber("Proximity", proximity.toDouble())
+
 
         }
     }
