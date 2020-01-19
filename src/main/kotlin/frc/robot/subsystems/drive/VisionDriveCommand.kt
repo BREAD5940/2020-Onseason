3c5fe43edba65b8e49f46b96f012e6976dd878d3
Annika Larson <annika@cole-larson.org>
2020-01-19 14:33:18 -0800

    vision

--- src/main/kotlin/frc/robot/subsystems/drive/VisionDriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/VisionDriveCommand.kt
@@ -6,7 +6,9 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.geometry.Transform2d
 import edu.wpi.first.wpilibj.geometry.Translation2d
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds
+import edu.wpi.first.wpilibj.trajectory.Trajectory
 import frc.robot.Constants
+import frc.robot.auto.paths.TrajectoryFactory
 import frc.robot.autonomous.paths.TrajectoryWaypoints
 import frc.robot.autonomous.paths.transformBy
 import frc.robot.subsystems.vision.VisionSubsystem
@@ -18,56 +20,18 @@ import org.ghrobotics.lib.mathematics.units.derived.radians
 import org.ghrobotics.lib.mathematics.units.derived.toRotation2d
 import org.ghrobotics.lib.mathematics.units.kFeetToMeter
 
-class VisionDriveCommand : FalconCommand(DriveSubsystem) {
-
-    lateinit var targetHeading: Rotation2d
-
-    override fun runsWhenDisabled() = true
-
-    private val rotationController = PIDController(10.0, 0.0, 0.0) // rad per sec per radian of error
-            .apply {
-                enableContinuousInput(-PI, PI)
-            }
-    private val translationController = PIDController(100.0, 0.0, 0.0)
-
-    val rotationRange = -2 * PI..2 * PI
-    val translationOutputRange = -4 * kFeetToMeter..4 * kFeetToMeter
-
-    override fun initialize() {
-        targetHeading = (importantAngles.minBy { (it - DriveSubsystem.robotPosition.rotation.radians).absoluteValue })!!.radians.toRotation2d()
-        rotationController.setpoint = targetHeading.radians
-        translationController.setpoint = 0.0
+val trajectory = TrajectoryFactory.grabThreeAndShoot
+val newPos = DriveSubsystem.robotPosition
+fun getEndHeading(): Rotation2d {
+    if(DriveSubsystem.robotPosition.minus(trajectory.states.last().poseMeters).translation.x < 1.0 && DriveSubsystem.robotPosition.minus(trajectory.states.last().poseMeters).translation.y < 1.0) {
+        return DriveSubsystem.robotPosition.rotation.plus( Rotation2d.fromDegrees(VisionSubsystem.xOffset))
+    } else {
+        return trajectory.states.last().poseMeters.rotation
     }
+}
 
+class VisionDriveCommand : FalconCommand() {
     override fun execute() {
-        val turn = rotationController.calculate(DriveSubsystem.robotPosition.rotation.radians).coerceIn(rotationRange)
-        val currentPose = DriveSubsystem.robotPosition
-        val targetPose: Pose2d
-        if (VisionSubsystem.hasTargets) {
-            targetPose = currentPose.transformBy(Transform2d(Translation2d(0.0, VisionSubsystem.yOffset), Rotation2d(VisionSubsystem.xOffset)))
-        } else {
-            targetPose = currentPose
-        }
-        // no clue if this works but here goes
-
-        var error = (targetPose.translation - currentPose.translation)
-        val targetVelocity = translationController.calculate(error.norm, 0.020).coerceIn(translationOutputRange)
-        error = error.normalize()
-        val vX = -error.x * targetVelocity
-        val vY = -error.y * targetVelocity
-
-        DriveSubsystem.periodicIO.output = SwerveDriveOutput.Velocity(
-                ChassisSpeeds.fromFieldRelativeSpeeds(vX, vY, turn, DriveSubsystem.robotPosition.rotation)
-        )
-
-        println("Target Heading ${targetHeading.degrees} turn $turn")
-    }
-
-    companion object {
-
-        val importantAngles = listOf(
-                // rocket n, bay and f (mirrored and not)
-                TrajectoryWaypoints.kSideStart.rotation /// TODO: have actual waypoints
-        ).map { it.radians }
+        DriveSubsystem.followTrajectory(trajectory, getEndHeading())
     }
 }
