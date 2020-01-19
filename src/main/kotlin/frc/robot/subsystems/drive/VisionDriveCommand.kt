6e6ffac96acef3b2b720eb121a246aebcc1de2bd
Annika Larson <annika@cole-larson.org>
2020-01-19 14:35:08 -0800

    remove unneeded imports

--- src/main/kotlin/frc/robot/subsystems/drive/VisionDriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/VisionDriveCommand.kt
@@ -1,24 +1,9 @@
 package frc.robot.subsystems.drive
 
-import edu.wpi.first.wpilibj.controller.PIDController
-import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.geometry.Rotation2d
-import edu.wpi.first.wpilibj.geometry.Transform2d
-import edu.wpi.first.wpilibj.geometry.Translation2d
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds
-import edu.wpi.first.wpilibj.trajectory.Trajectory
-import frc.robot.Constants
 import frc.robot.auto.paths.TrajectoryFactory
-import frc.robot.autonomous.paths.TrajectoryWaypoints
-import frc.robot.autonomous.paths.transformBy
 import frc.robot.subsystems.vision.VisionSubsystem
-import kotlin.math.PI
-import kotlin.math.absoluteValue
-import lib.normalize
 import org.ghrobotics.lib.commands.FalconCommand
-import org.ghrobotics.lib.mathematics.units.derived.radians
-import org.ghrobotics.lib.mathematics.units.derived.toRotation2d
-import org.ghrobotics.lib.mathematics.units.kFeetToMeter
 
 val trajectory = TrajectoryFactory.grabThreeAndShoot
 val newPos = DriveSubsystem.robotPosition
