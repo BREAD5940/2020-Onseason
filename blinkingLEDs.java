3c33eccead7843994b07420c9d2871a377ae56cc
afurlan7 <55670341+afurlan7@users.noreply.github.com>
2020-01-19 14:47:36 -0800

    Create blinkingLEDs.java

---
+++ blinkingLEDs.java
@@ -0,0 +1,11 @@
+public class solidLEDs {
+
+  public static int blinkNumber = 6;
+  public static double blinkTime = 0.1; 
+  public static double totalBlinkTime = blinkNumber * blinkTime;
+  
+  public static void main(String args[]){
+    System.out.println(totalBlinkTime);
+  }
+
+}
