04d7447c79ccca6a10984a1b1729772ec202aa15
lynnf <56235486+lfurlan2@users.noreply.github.com>
2020-02-09 14:42:59 -0800

    here's led stuff :)

--- src/main/kotlin/frc/robot/subsystems/superstructure/LEDs.kt
+++ src/main/kotlin/frc/robot/subsystems/superstructure/LEDs.kt
@@ -1,9 +1,43 @@
 package frc.robot.subsystems.superstructure
 
-object LEDs {
+import edu.wpi.first.wpilibj.AddressableLED
+import org.ghrobotics.lib.mathematics.units.SIUnit
+import org.ghrobotics.lib.mathematics.units.Second
+import java.awt.Color
+import kotlin.concurrent.thread
+import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import org.ghrobotics.lib.commands.FalconSubsystem
+import org.ghrobotics.lib.mathematics.units.millisecond
 
-    override fun robotInit() {
+class TheLEDs : FalconSubsystem() {
+    object LEDs : AddressableLED(9) //
+    object LEDBuffer : AddressableLEDBuffer(100)
 
+    override fun lateInit() {
+        LEDs.setLength(LEDBuffer.length)
+        LEDs.setData(LEDBuffer)
+        LEDs.start()
+
+        updateThread.start()
     }
 
+    private val updateThread = thread {
+        var firstPixelValue = 0
+
+        while (true) {
+            // For every pixel
+            for (i in 0..LEDBuffer.length) {
+                val value = (firstPixelValue + (i * 255 / LEDBuffer.length)) % 255
+                // Set the value
+                LEDBuffer.setHSV(i, 0, 255, value)
+            }
+
+            // Increase by to make the rainbow "move"
+            firstPixelValue += 3
+            // Check bounds
+            firstPixelValue %= 255
+        }
+
+        // Thread.sleep(wantedState.blinkTime.millisecond.toLong() / 2)
+    }
 }
