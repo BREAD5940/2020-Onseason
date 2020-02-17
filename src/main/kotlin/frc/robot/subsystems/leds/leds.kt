6afba71371ac60fe8e61049cb2908aa52435722d
5940 User <5940user@DMC-3s-MacBook-Pro.local>
2020-02-16 16:18:26 -0800

    Create leds.kt

---
+++ src/main/kotlin/frc/robot/subsystems/leds/leds.kt
@@ -0,0 +1,12 @@
+import edu.wpi.first.wpilibj.AddressableLED
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+fun ledsRun() {
+
+    object led : AddressableLed(9)
+    object buffer : AddressableLedBuffer(100)
+
+    led.setLength
+
+}
