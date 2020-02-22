2bcccb8be6f3351e4073b5d2109e3990ff780142
5940 User <5940user@DMC-3s-MacBook-Pro.local>
2020-02-21 16:20:41 -0800

    hi

--- src/main/kotlin/frc/robot/subsystems/leds/leds.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/leds.kt
@@ -9,11 +9,77 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer
 import org.ghrobotics.lib.commands.FalconSubsystem
 import org.ghrobotics.lib.mathematics.units.millisecond
 
-fun ledsRun() {
+fun ledsRun(var system) {
 
     object led : AddressableLed(9)
     object buffer : AddressableLedBuffer(100)
 
-    led.setLength
+    led.setLength(buffer.getLength())
+
+    if(system == "intake") {
+        while(true) {
+            //turn on orange
+            for(var i = 0. i <= buffer.getLength. i++) {
+                buffer.setHSV(i, 30, 100, 100)
+            }
+            led.setData(buffer)
+
+            //turn off for half a sec
+            Timer().schedule(500) {
+                for(var i = 0. i <= buffer.getLength. i++) {
+                buffer.setHSV(i, 30, 100, 0)
+                }
+                led.setData(buffer)
+            }
+        }
+    } else if(system == "shooting") {
+        while(true) {
+            //turn on green
+            for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 110, 100, 100)
+            }
+            led.setData(buffer)
+
+            //turn off for half a sec
+            Timer().schedule(500) {
+                for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 110, 100, 0)
+            }
+                led.setData(buffer)
+            }
+        }
+    } else if(system == "climbing") {
+        while(true) {
+            //turn on purple
+            for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 275, 100, 100)
+            }
+            led.setData(buffer)
+
+            //turn off for half a sec
+            Timer().schedule(500) {
+                for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 275, 100, 0)
+            }
+                led.setData(buffer)
+            }
+        }
+    } else {
+        while(true) {
+            //turn on red
+            for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 0, 100, 100)
+            }
+            led.setData(buffer)
+
+            //turn off for half a sec
+            Timer().schedule(500) {
+                for (var i = 0.i <= buffer.getLength.i++) {
+                buffer.setHSV(i, 0, 100, 0)
+            }
+                led.setData(buffer)
+            }
+        }
+    }
 
 }
