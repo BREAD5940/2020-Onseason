ba1075c69b8c2c59c30231f22553a61ab9f09ac9
lynnf <56235486+lfurlan2@users.noreply.github.com>
2020-02-25 21:13:51 -0800

    here's more led stuff :)

--- src/main/kotlin/frc/robot/subsystems/superstructure/LEDs.kt
+++ src/main/kotlin/frc/robot/subsystems/superstructure/LEDs.kt
@@ -6,37 +6,56 @@ import org.ghrobotics.lib.mathematics.units.Second
 import java.awt.Color
 import kotlin.concurrent.thread
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.DriverStation
 import org.ghrobotics.lib.commands.FalconSubsystem
+import frc.robot.Controls
 import org.ghrobotics.lib.mathematics.units.millisecond
 
 class TheLEDs : FalconSubsystem() {
     object LEDs : AddressableLED(9) //
     object LEDBuffer : AddressableLEDBuffer(100)
+    var allianceHue = 85 // GREEN (120/360 on color wheel)
 
     override fun lateInit() {
         LEDs.setLength(LEDBuffer.length)
         LEDs.setData(LEDBuffer)
         LEDs.start()
 
+        if (DriverStation.getInstance().alliance == DriverStation.Alliance.Red) {
+            allianceHue = 0      // RED (0/360 on color wheel)
+        } else if (DriverStation.getInstance().alliance == DriverStation.Alliance.Blue) {
+            allianceHue = 170    // BLUE (240/360 on color wheel)
+        }
+
         updateThread.start()
     }
 
     private val updateThread = thread {
         var firstPixelValue = 0
+        var hue = allianceHue
+
 
         while (true) {
             // For every pixel
             for (i in 0..LEDBuffer.length) {
                 val value = (firstPixelValue + (i * 255 / LEDBuffer.length)) % 255
                 // Set the value
-                LEDBuffer.setHSV(i, 0, 255, value)
+                LEDBuffer.setHSV(i, hue, 255, value)
             }
 
             // Increase by to make the rainbow "move"
             firstPixelValue += 3
             // Check bounds
             firstPixelValue %= 255
+
+            if (Controls.isClimbing) {
+                hue = 277
+            } else {
+                hue = allianceHue
+            }
+
         }
+
 
         // Thread.sleep(wantedState.blinkTime.millisecond.toLong() / 2)
     }
