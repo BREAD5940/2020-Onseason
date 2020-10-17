c3455b7bd1dc4851f04cfc628902b8a911c9c782
dmc-account <>
2020-10-17 16:06:59 -0700

    revert to pre cvr code for 8 pc auto

--- src/main/kotlin/frc/robot/autonomous/routines/EightPCFromTrenchRoutine.kt
+++ src/main/kotlin/frc/robot/autonomous/routines/EightPCFromTrenchRoutine.kt
@@ -29,24 +29,26 @@ class EightPCFromTrenchRoutine : AutoRoutine() {
 
             +instantCommand { DriveSubsystem.robotPosition = Pose2d(path1.states.first().poseMeters.translation, 180.degrees.toRotation2d()) }
 
-            +(FlywheelSubsystem.agitateAndShoot(1.seconds))
-                    .deadlineWith(VisionDriveCommand())
-
-            +DriveSubsystem.followTrajectory(path1) { 180.degrees.toRotation2d() }
+            +DriveSubsystem.followTrajectory(path1) { -166.degrees.toRotation2d() }
                     .alongWith(IntakeSubsystem.extendIntakeCommand())
+
+            +(FlywheelSubsystem.agitateAndShoot(3.seconds))
+                    .deadlineWith(VisionDriveCommand())
 
             +PointTurnCommand(0.degrees.toRotation2d()) //TODO figure out if this turns robot 0 or if it turns to 0, assumed latter
 
             +DriveSubsystem.followTrajectory2(path2) { 0.degrees }
                     .alongWith(runCommand(IntakeSubsystem) { IntakeSubsystem.setSpeed(1.0) })
-                    //.andThen(Runnable { IntakeSubsystem.setNeutral() }, IntakeSubsystem) TODO when does it know when to switch over? there's no time constraint that tells it when to switch
+                    .andThen(Runnable { IntakeSubsystem.setNeutral() }, IntakeSubsystem) //TODO when does it know when to switch over? there's no time constraint that tells it when to switch
 
             +PointTurnCommand(180.degrees.toRotation2d())
-                    .andThen (Runnable { IntakeSubsystem.setNeutral() }, IntakeSubsystem)
+                    //.andThen (Runnable { IntakeSubsystem.setNeutral() }, IntakeSubsystem)
 
-            +DriveSubsystem.followTrajectory(path3) { 180.degrees.toRotation2d() }
+            +DriveSubsystem.followTrajectory(path3) { -166.degrees.toRotation2d() }
 
             +(FlywheelSubsystem.agitateAndShoot(3.seconds))
-                    .deadlineWith(VisionDriveCommand())
+                    .deadlineWith(VisionDriveCommand(),
+                        runCommand(IntakeSubsystem) { IntakeSubsystem.setSpeed(1.0); IntakeSubsystem.setSmolPistonExtension(true) })
+                    .andThen(Runnable { IntakeSubsystem.setNeutral() }, IntakeSubsystem)
         }
 }
