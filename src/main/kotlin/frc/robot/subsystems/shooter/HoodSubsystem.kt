0fb8faba0d99291a4e90d0eea89f19364a64b8af
Matt <matthew.morley.ca@gmail.com>
2021-10-09 17:38:53 -0400

    Remove hood absolute encoder, replace with zeroing from bagel

--- src/main/kotlin/frc/robot/subsystems/shooter/HoodSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/HoodSubsystem.kt
@@ -1,22 +1,24 @@
 package frc.robot.subsystems.shooter
 
 import com.revrobotics.CANSparkMaxLowLevel
-import edu.wpi.first.wpilibj.*
 import edu.wpi.first.wpilibj.controller.PIDController
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile
 import frc.robot.Ports
-import kotlin.math.PI
-import kotlin.math.absoluteValue
 import org.ghrobotics.lib.commands.FalconSubsystem
-import org.ghrobotics.lib.mathematics.units.derived.*
+import org.ghrobotics.lib.mathematics.units.derived.degrees
+import org.ghrobotics.lib.mathematics.units.derived.inRadians
 import org.ghrobotics.lib.mathematics.units.nativeunit.NativeUnitRotationModel
 import org.ghrobotics.lib.mathematics.units.nativeunit.nativeUnits
 import org.ghrobotics.lib.motors.rev.falconMAX
 
 object HoodSubsystem : FalconSubsystem() {
 
-    val hoodMotor = falconMAX(Ports.shooterHoodId, CANSparkMaxLowLevel.MotorType.kBrushless, NativeUnitRotationModel((1.0 * 5.0 / 12.0 * 385.0).nativeUnits)) {
+    val hoodMotor = falconMAX(
+        Ports.shooterHoodId,
+        CANSparkMaxLowLevel.MotorType.kBrushless,
+        NativeUnitRotationModel((1.0 * 5.0 / 12.0 * 385.0).nativeUnits)
+    ) {
         with(canSparkMax) {
             restoreFactoryDefaults()
             setSecondaryCurrentLimit(35.0)
@@ -25,9 +27,10 @@ object HoodSubsystem : FalconSubsystem() {
         controller.p = 0.3
     }
 
-    private val hoodAngleEncoder = AnalogInput(Ports.hoodEncoderPort)
-    val hoodAngle
-        get() = ((hoodAngleEncoder.voltage / RobotController.getVoltage5V() * 2.0 * PI).radians + 3.degrees) // more offset = lower hood = more curve shot
+    //    private val hoodAngleEncoder = AnalogInput(Ports.hoodEncoderPort)
+//    val hoodAngle
+//        get() = ((hoodAngleEncoder.voltage / RobotController.getVoltage5V() * 2.0 * PI).radians + 3.degrees) // more offset = lower hood = more curve shot
+    val hoodAngle get() = hoodMotor.encoder.position
 
     var wantedAngle = 55.degrees
 
@@ -52,26 +55,43 @@ object HoodSubsystem : FalconSubsystem() {
     var wasInOnSPARKClosedLoop = false
     var lastBrushlessEncoderAngle = 0.degrees
 
+    sealed class State {
+        object Homing : State()
+        object Position : State()
+    }
+
+    var wantedState: State = State.Homing
+
     override fun periodic() {
 
-        val setpoint = TrapezoidProfile(constraints, TrapezoidProfile.State(wantedAngle.coerceIn(safeHoodAngles).value, 0.0), lastProfiledReference)
-                .calculate(0.020)
-
-        lastProfiledReference = setpoint
-
-        if ((hoodAngle - wantedAngle).absoluteValue > 4.degrees) {
-            hoodMotor.setDutyCycle(hoodPidController.calculate(hoodAngle.inRadians(), setpoint.position))
-            wasInOnSPARKClosedLoop = false
-        } else {
-
-            if (!wasInOnSPARKClosedLoop) {
-                wasInOnSPARKClosedLoop = true
-                hoodMotor.encoder.resetPosition(hoodAngle)
-//                lastBrushlessEncoderAngle = hoodMotor.encoder.position
+        when (wantedState) {
+            is State.Homing -> {
             }
+            is State.Position -> {
+                val setpoint = TrapezoidProfile(
+                    constraints,
+                    TrapezoidProfile.State(wantedAngle.coerceIn(safeHoodAngles).value, 0.0),
+                    lastProfiledReference
+                )
+                    .calculate(0.020)
 
-            // hold the last encoder angle
-            hoodMotor.setPosition(wantedAngle)
+                lastProfiledReference = setpoint
+
+//                if ((hoodAngle - wantedAngle).absoluteValue > 4.degrees) {
+//                    hoodMotor.setDutyCycle(hoodPidController.calculate(hoodAngle.inRadians(), setpoint.position))
+//                    wasInOnSPARKClosedLoop = false
+//                } else {
+//
+//                    if (!wasInOnSPARKClosedLoop) {
+//                        wasInOnSPARKClosedLoop = true
+//                        hoodMotor.encoder.resetPosition(hoodAngle)
+// //                lastBrushlessEncoderAngle = hoodMotor.encoder.position
+//                    }
+//
+//                    // hold the last encoder angle
+                hoodMotor.setPosition(wantedAngle)
+//            }
+            }
         }
     }
 }
---
+++ src/main/kotlin/frc/robot/subsystems/shooter/ZeroCommand.kt
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.shooter
+
+import edu.wpi.first.wpilibj.Timer
+import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.mathematics.units.amps
+import org.ghrobotics.lib.mathematics.units.derived.degrees
+import org.ghrobotics.lib.mathematics.units.derived.velocity
+import org.ghrobotics.lib.mathematics.units.derived.volts
+
+class ZeroCommand : FalconCommand(HoodSubsystem) {
+
+    val timer = Timer()
+
+    override fun initialize() {
+        super.initialize()
+        HoodSubsystem.hoodMotor.smartCurrentLimit = 3.amps
+        HoodSubsystem.hoodMotor.canSparkMax.setSecondaryCurrentLimit(5.0)
+        HoodSubsystem.wantedState = HoodSubsystem.State.Homing
+        HoodSubsystem.hoodMotor.setVoltage(3.volts)
+
+        timer.reset()
+        timer.start()
+    }
+
+    override fun isFinished(): Boolean {
+        return HoodSubsystem.hoodMotor.encoder.velocity.absoluteValue < 10.degrees.velocity &&
+                timer.get() > 0.25
+    }
+
+    override fun end(interrupted: Boolean) {
+        super.end(interrupted)
+        HoodSubsystem.wantedState = HoodSubsystem.State.Position
+        HoodSubsystem.hoodMotor.encoder.resetPosition(17.degrees)
+    }
+}
